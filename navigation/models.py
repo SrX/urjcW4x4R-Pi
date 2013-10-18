a523903de71341bbfc398331e5114bef7fe20926
Serhii Slipenko <LeSagra@gmail.com>
2013-10-18 16:55:21 +0200

    Creado el modelo de la ruta y de la coordenada

--- navigation/models.py
+++ navigation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Route(models.Model):
+    name = models.CharField(max_length=80)
+
+class Coord(models.Model):
+    route = models.ForeignKey(Route)
+    lat = models.DecimalField(max_digits=20, decimal_places=10)
+    lon = models.DecimalField(max_digits=20, decimal_places=10)
+    speed = models.DecimalField(max_digits=7, decimal_places=4);
+    track = models.DecimalField(max_digits=10, decimal_places=5);
+    time = models.CharField(max_length=80)
