d9a409f45c2d3a9ef910f5f452f4d6c2f9aca10a
samartin <samuel.martinm@gmail.com>
2013-11-08 16:44:19 +0100

    cambios prueba init.d

--- variables/__init__.py
+++ variables/__init__.py
@@ -1,12 +1,14 @@
 import gps
 import sys
+import serial
+import time
 
-try:
-	_gps = gps.gps(mode=gps.WATCH_ENABLE|gps.WATCH_JSON|gps.WATCH_NEWSTYLE)
-except: #except (StopIteration):
-    print "Unexpected error:", sys.exc_info()[0]
-    _gps = {}
-    pass
+#try:
+#	_gps = gps.gps(mode=gps.WATCH_ENABLE|gps.WATCH_JSON|gps.WATCH_NEWSTYLE)
+#except: #except (StopIteration):
+#    print "Unexpected error:", sys.exc_info()[0]
+#    _gps = {}
+#    pass
 
 
 # def resetGps():
@@ -18,7 +20,25 @@ except: #except (StopIteration):
 #         pass
 
 
-import serial, time
+class Start(object):
+    
+    def __init__(self):
+        self._gps = gps.gps(mode=gps.WATCH_ENABLE|gps.WATCH_NEWSTYLE)
+    
+    def Update(self):
+        try:
+            self._gps.next()
+        except StopIteration:
+            pass                #Hay que decidir que hacer si se produce un error.
+
+        return  {   'lat'   :   self._gps.fix.latitude,
+                    'lon'   :   self._gps.fix.longitude,
+                    'track' :   self._gps.fix.track,
+                    'speed' :   self._gps.fix.speed,
+                    'time'  :   self._gps.fix.time,         #le pasa algo raro
+                    #'date'     :   self._gps.fix.date,
+                    #'alt'  :   self._gps.fix.altitude,
+                     }
 
 class Control(object):
 
@@ -49,9 +69,9 @@ class Control(object):
             self.arduino_conect.write(chr(1))
             self.arduino_conect.write(chr(grados))
 
-
 try:
     vehicle = Control();
+    stats = GPS.Start();
 except: 
     print "Unexpected error:", sys.exc_info()[0]
     pass
