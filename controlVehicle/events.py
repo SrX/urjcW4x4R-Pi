92d5c0201a155d599587b97caaaad4fa9f03907c
samartin <samuel.martinm@gmail.com>
2013-11-15 20:44:25 +0100

    Comunicacion inicial ajusta los valores de velocidad, giro y creacion de ruta

--- controlVehicle/events.py
+++ controlVehicle/events.py
@@ -2,41 +2,38 @@ from django_socketio import events
 from django_socketio import broadcast_channel
 
 from controlVehicle import *
+from controlVehicle import RecordThread
 from navigation.models import Route, Coord;
 from navigation import _gps;
+from navigation import _thrd
 import time
 import threading
-
-def from_gps_to_bdd():
-    intervalo = 8;
-    nameroute = "Ruta prueba"
-    try:
-        # rout = Route.objects.create(name="Route " + str(var.nroute));
-        rout = Route.objects.create(name=nameroute);
-        print "Guardando en base de datos nueva ruta.."
-        for i in range(1, 400):
-            if stop_grabado == 1:
-                threading.exit();
-            cp = _gps.update();
-            if (i % intervalo) == 0 and float(cp['lon']) != 0.0:
-                cor = Coord.objects.create(route=rout, lat=float(cp['lat']), lon=float(cp['lon']), track=float(cp['track']), speed=float(cp['speed']), time=cp['time']);
-        # var.nroute+=1;
-        print "Nueva ruta guardada en base de datos"
-    except:
-        raise
 
 @events.on_message(channel="hand_control")
 def message(request, socket, context, message):
     print "manual_control"
     try:
         if message['action'] == 'startroute':
-            print "EMPIEZO GRABADO"
-            t = threading.Thread(target=from_gps_to_bdd)
-            t.start()
+            if not _thrd.has_key('RecordThread'):
+                _thrd['RecordThread'] = RecordThread()
+                _thrd['RecordThread'].setDaemon(True)
+                _thrd['RecordThread'].start()
+                rec.recording=1
+                socket.send_and_broadcast_channel({'action':'startedroute'}) #tabular
+        elif message['action'] == 'stoproute':
+            _thrd['RecordThread'].stop()
+            del _thrd['RecordThread']
+            print "Nueva ruta guardada en base de datos"
+            rec.recording=0
+            socket.send_and_broadcast_channel({'action':'stoppedroute'})
+        elif message['action'] == 'init':
+            print str(vehicle.ws_value)
+            print str(rec.recording)
+            ret = {'action':'init', 'ws': vehicle.ws_value, 'ad': vehicle.ad_value, 'recording': rec.recording};
+            socket.send_and_broadcast_channel(ret)
         else:
             ws_value, ad_value = vehicle.action(message['action'])
             ret = {'action':'update', 'ws': ws_value, 'ad':ad_value};
-            
             socket.send_and_broadcast_channel(ret)
     except:
         raise
