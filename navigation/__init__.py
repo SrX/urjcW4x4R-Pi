7d493079396f761bc79edc2a9b0e3d8a299f23e1
samartin <samuel.martinm@gmail.com>
2013-11-22 19:44:14 +0100

    Codigo dejado como estaba

--- navigation/__init__.py
+++ navigation/__init__.py
@@ -127,82 +127,6 @@ class RouteState(object):
         self.started = 0
         self.id = -1
 
-class Control(object):
-    def __init__ (self):
-        self.ws_value = 90
-        self.ad_value = 90
-        
-        # Paso del incremento
-        self.inc = 1;
-        
-        try:
-            self.arduino_conect = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-        except:  # except (StopIteration):
-            print "Unexpected error: Control ", sys.exc_info()[0]
-            self.arduino_conect = ''
-            pass
-        
-        self.reset()
-        
-        
-    def reset(self):
-        self.ws_value = 90
-        self.ad_value = 90
-        self.set(self.ws_value, self.ad_value)
-        
-    def action(self, action):
-        if action == 'w':
-            self.ws_value += self.inc
-        elif action == 's':
-            self.ws_value -= self.inc
-        elif action == 'd':
-            self.ad_value += self.inc
-        elif action == 'a':
-            self.ad_value -= self.inc
-        else:  # action == 'q':
-            # por medidas de seguridad, si el coche se escapa 
-            # y/o el piloto se pone nervioso
-            self.reset()
-        
-        self.evalue_wa()
-        self.set(self.ws_value, self.ad_value)
-        
-        #devolver el valor, el que se ha pasado al coche
-        return self.ws_value, self.ad_value
-            
-    def set(self, speed, angle):
-        
-
-        print 'Vehicle: s-' + str(speed) + ' a-' + str(angle)
-        
-        if self.arduino_conect != '':
-            self.speed(speed)
-            self.turn(angle)
-        
-    def speed(self, velocidad):
-            self.arduino_conect.write(chr(255))
-            self.arduino_conect.write(chr(2))
-            self.arduino_conect.write(chr(velocidad))
-
-
-    def turn(self, grados):
-            self.arduino_conect.write(chr(255)) 
-            self.arduino_conect.write(chr(1))
-            self.arduino_conect.write(chr(grados))
-            
-    def evalue_wa(self):
-        if self.ws_value > 120:
-            self.ws_value = 120
-        elif self.ws_value < 60:
-            self.ws_value = 60
-        
-        if self.ad_value > 120:
-            self.ad_value = 120
-        elif self.ad_value < 60:
-            self.ad_value = 60
-
-
-
 _thrd = dict()
 
 _gps = StartGps();
@@ -212,5 +136,3 @@ bth.setDaemon(True)
 bth.start()
 
 rs = RouteState()
-
-vehicle = Control()
