8c7509fe749a95b4e5e6f21ca0169f627d5a0dd9
samartin <samuel.martinm@gmail.com>
2013-11-22 19:33:57 +0100

    Añadido codigo para que funcione en el coche la navegacion, cambiado intervalo del mando para hacer pruebas y reorganizado codigo entre los init de controlvehicle y navigation

--- controlVehicle/__init__.py
+++ controlVehicle/__init__.py
@@ -3,8 +3,8 @@ import sys
 import serial
 import time
 import threading
-from navigation.models import Route, Coord;
-from navigation import _gps;
+from navigation.models import Route, Coord
+from navigation import _gps
 
 def is_integer(s):
     try:
@@ -19,81 +19,6 @@ def name_used(s):
         return True
     except:
         return False
-
-
-class Control(object):
-    def __init__ (self):
-        self.ws_value = 90
-        self.ad_value = 90
-        
-        # Paso del incremento
-        self.inc = 3;
-        
-        try:
-            self.arduino_conect = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-        except:  # except (StopIteration):
-            print "Unexpected error: Control ", sys.exc_info()[0]
-            self.arduino_conect = ''
-            pass
-        
-        self.reset()
-        
-        
-    def reset(self):
-        self.ws_value = 90
-        self.ad_value = 90
-        self.set(self.ws_value, self.ad_value)
-        
-    def action(self, action):
-        if action == 'w':
-            self.ws_value += self.inc
-        elif action == 's':
-            self.ws_value -= self.inc
-        elif action == 'd':
-            self.ad_value += self.inc
-        elif action == 'a':
-            self.ad_value -= self.inc
-        else:  # action == 'q':
-            # por medidas de seguridad, si el coche se escapa 
-            # y/o el piloto se pone nervioso
-            self.reset()
-        
-        self.evalue_wa()
-        self.set(self.ws_value, self.ad_value)
-        
-        #devolver el valor, el que se ha pasado al coche
-        return self.ws_value, self.ad_value
-            
-    def set(self, speed, angle):
-        
-
-        print 'Vehicle: s-' + str(speed) + ' a-' + str(angle)
-        
-        if self.arduino_conect != '':
-            self.speed(speed)
-            self.turn(angle)
-        
-    def speed(self, velocidad):
-            self.arduino_conect.write(chr(255))
-            self.arduino_conect.write(chr(2))
-            self.arduino_conect.write(chr(velocidad))
-
-
-    def turn(self, grados):
-            self.arduino_conect.write(chr(255)) 
-            self.arduino_conect.write(chr(1))
-            self.arduino_conect.write(chr(grados))
-            
-    def evalue_wa(self):
-        if self.ws_value > 120:
-            self.ws_value = 120
-        elif self.ws_value < 60:
-            self.ws_value = 60
-        
-        if self.ad_value > 120:
-            self.ad_value = 120
-        elif self.ad_value < 60:
-            self.ad_value = 60
 
 class RecordThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
@@ -129,5 +54,4 @@ class Rec(object):
     def __init__ (self):
         self.recording = 0
 
-vehicle = Control()
 rec = Rec()
--- navigation/__init__.py
+++ navigation/__init__.py
@@ -86,6 +86,10 @@ class RouteThread(threading.Thread):
             rout = Route.objects.get(id=route_id)
             coords = Route.get_only_coord(rout)
             for point in coords:
+                try:
+                    vehicle.speed(96)#velocidad minima
+                except:
+                    pass
                 reached = False;
                 infopoint={'action':'next_point', 'lat': point[0], 'lon': point[1]}
                 broadcast_channel(infopoint, 'navigation')
@@ -96,6 +100,10 @@ class RouteThread(threading.Thread):
                         H=heading_to(point, gpsData)
                         angle_diff = get_angle_diff(gpsData['track'], H)
                         turn_angle = angle_to_turn_angle(angle_diff) #lo devuelve como int
+                        try:
+                            vehicle.turn(turn_angle)
+                        except:
+                            pass
                         infopoint={'action':'state_route', 'lat': gpsData['lat'], 'lon': gpsData['lon'], 'dist': dist}
                         broadcast_channel(infopoint, 'navigation')
                         print 'gpsData: ' + str(gpsData['lat']) + ' ' + str(gpsData['lon']) + ' Next point: ' + str(point) + ' Distance: ' +str(dist) + ' Turn angle: ' + str(turn_angle)
@@ -108,6 +116,7 @@ class RouteThread(threading.Thread):
                 _thrd['RouteThread'].stop()
                 del _thrd['RouteThread']
                 rs.started=0
+                vehicle.reset()
                 broadcast_channel({'action':'routeIsStopped'}, 'navigation')
       
         except:
@@ -118,6 +127,82 @@ class RouteState(object):
         self.started = 0
         self.id = -1
 
+class Control(object):
+    def __init__ (self):
+        self.ws_value = 90
+        self.ad_value = 90
+        
+        # Paso del incremento
+        self.inc = 1;
+        
+        try:
+            self.arduino_conect = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+        except:  # except (StopIteration):
+            print "Unexpected error: Control ", sys.exc_info()[0]
+            self.arduino_conect = ''
+            pass
+        
+        self.reset()
+        
+        
+    def reset(self):
+        self.ws_value = 90
+        self.ad_value = 90
+        self.set(self.ws_value, self.ad_value)
+        
+    def action(self, action):
+        if action == 'w':
+            self.ws_value += self.inc
+        elif action == 's':
+            self.ws_value -= self.inc
+        elif action == 'd':
+            self.ad_value += self.inc
+        elif action == 'a':
+            self.ad_value -= self.inc
+        else:  # action == 'q':
+            # por medidas de seguridad, si el coche se escapa 
+            # y/o el piloto se pone nervioso
+            self.reset()
+        
+        self.evalue_wa()
+        self.set(self.ws_value, self.ad_value)
+        
+        #devolver el valor, el que se ha pasado al coche
+        return self.ws_value, self.ad_value
+            
+    def set(self, speed, angle):
+        
+
+        print 'Vehicle: s-' + str(speed) + ' a-' + str(angle)
+        
+        if self.arduino_conect != '':
+            self.speed(speed)
+            self.turn(angle)
+        
+    def speed(self, velocidad):
+            self.arduino_conect.write(chr(255))
+            self.arduino_conect.write(chr(2))
+            self.arduino_conect.write(chr(velocidad))
+
+
+    def turn(self, grados):
+            self.arduino_conect.write(chr(255)) 
+            self.arduino_conect.write(chr(1))
+            self.arduino_conect.write(chr(grados))
+            
+    def evalue_wa(self):
+        if self.ws_value > 120:
+            self.ws_value = 120
+        elif self.ws_value < 60:
+            self.ws_value = 60
+        
+        if self.ad_value > 120:
+            self.ad_value = 120
+        elif self.ad_value < 60:
+            self.ad_value = 60
+
+
+
 _thrd = dict()
 
 _gps = StartGps();
@@ -127,3 +212,5 @@ bth.setDaemon(True)
 bth.start()
 
 rs = RouteState()
+
+vehicle = Control()
